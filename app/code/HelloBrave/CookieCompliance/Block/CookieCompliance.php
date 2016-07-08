ead7c0da8c9416010a76199c008bf77034aa05c6
Daniel Farmer <dan@hellobrave.com>
2016-07-08 13:32:56 +0100

    V1.0.0 - submitted to Magento Marketplace

---
+++ app/code/HelloBrave/CookieCompliance/Block/CookieCompliance.php
@@ -0,0 +1,7 @@
+<?php namespace HelloBrave\CookieCompliance\Block;
+ 
+class CookieCompliance extends \Magento\Framework\View\Element\Template {
+	public function _prepareLayout() {
+		return parent::_prepareLayout();
+	}
+}
